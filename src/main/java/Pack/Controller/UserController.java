8c6babae96070de8ca2e95afaa2675e58a5dc8ed
shjang <wkdtndgns@naver.com>
2023-06-23 09:38:40 +0900

    temp

--- src/main/java/Pack/Controller/UserController.java
+++ src/main/java/Pack/Controller/UserController.java
@@ -2,14 +2,25 @@ package Pack.Controller;
 
 import Pack.Entity.User;
 import Pack.Service.UserService;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.net.URI;
+import java.net.URISyntaxException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
@@ -39,4 +50,30 @@ public class UserController {
     public void deleteUser(@PathVariable String id) {
         userService.deleteUser(id);
     }
+
+    @PostMapping("/uploadImage")
+    public String uploadImage(@RequestParam("image") MultipartFile image) {
+        try {
+            if (image.isEmpty()) {
+                return "No image file provided.";
+            }
+
+            // 파일명 생성
+            String fileName = StringUtils.cleanPath(image.getOriginalFilename());
+
+            // 이미지를 저장할 경로 지정
+            String uploadDir = "src/main/resources/static/img";
+            String filePath = uploadDir + "/" + fileName;
+
+            // 이미지를 저장
+            Path path = Paths.get(filePath);
+//            Files.createDirectories(path.getParent());
+            Files.write(path, image.getBytes());
+            return "Image uploaded successfully!";
+        } catch (IOException  e) {
+            // 저장 중 오류 발생 시 처리
+            return "Error occurred while uploading the image.";
+        }
+    }
+
 }
--- src/main/java/Pack/SpringBoot4Application.java
+++ src/main/java/Pack/SpringBoot4Application.java
@@ -2,12 +2,18 @@ package Pack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBoot4Application {
+public class SpringBoot4Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBoot4Application.class, args);
-    }
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(SpringBoot4Application.class);
+  }
 
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBoot4Application.class, args);
+  }
 }
